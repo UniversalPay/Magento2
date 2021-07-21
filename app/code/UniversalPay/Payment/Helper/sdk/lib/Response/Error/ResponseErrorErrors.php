012610806c1678527e153fcec35124323475bb4b
Eóin O' Leary <eoin.oleary@evopayments.com>
2021-07-21 07:53:39 +0100

    Update ResponseErrorErrors.php

--- app/code/UniversalPay/Payment/Helper/sdk/lib/Response/Error/ResponseErrorErrors.php
+++ app/code/UniversalPay/Payment/Helper/sdk/lib/Response/Error/ResponseErrorErrors.php
@@ -6,7 +6,7 @@ use UniversalPay\Payment\Helper\sdk\lib\Response;
 
 class ResponseErrorErrors extends Response {
 
-<<<<<<< HEAD
+
     public function __construct($errors = array())
         {
             if (is_array($errors)) {
@@ -21,7 +21,7 @@ class ResponseErrorErrors extends Response {
                 $this->_data['errors'] = $errors;
             }
         }
-=======
+
     public function __construct($errors = array()) {
         if (is_array($errors)) {
             foreach ($errors as $error) {
@@ -31,6 +31,6 @@ class ResponseErrorErrors extends Response {
             $this->_data[$errors] = $errors;
         }
     }
->>>>>>> a4e3308eab793fa14e98745bdcf7ae517729efae
+
 
 }
